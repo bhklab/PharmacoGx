d09b275566cc5a079f3696fd7034015297cba3d0
Petr Smirnov <petr.smirnov@sri.utoronto.ca>
2016-04-18 11:03:21 -0400

    fixed t/f in vignette

--- R/drugPerturbationSig.R
+++ R/drugPerturbationSig.R
@@ -35,6 +35,7 @@
 #'   should the computation be parallelized over?
 #' @param returnValues [character] Which of estimate, t-stat, p-value and fdr
 #'   should the function return for each gene drug pair?
+#' @param verbose [bool] Should diagnostive messages be printed? (default false)
 #' @return [list] a 3D array with genes in the first dimension, drugs in the
 #'   second, and return values in the third.
 #' @export
