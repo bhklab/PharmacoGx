4cbc151f20426dcd62eefeba8dd1cb2abe18835c
psmirnov <psmirnov2000@gmail.com>
2016-05-13 14:59:48 -0400

    Small changes to amcc

--- R/AdaptiveMatthewCor.R
+++ R/AdaptiveMatthewCor.R
@@ -23,21 +23,23 @@ amcc <-
         return (res)
       }, iix=iix, x2=x2, y2=y2)
       mm <- do.call(rbind, mcres)
+      mode(mm) <- "numeric"
       ## remove extreme indices
       rmix <- c(1:(min.cat - 1), (nrow(mm) - min.cat + 2):nrow(mm))
-      mccix <- max(which(mm[-rmix, "mcc"] == max(mm[-rmix, "mcc"], na.rm=TRUE))) + (min.cat - 1)
+      mccix <- max(which(mm[-rmix, "estimate", drop=FALSE] == max(mm[-rmix, "estimate", drop=FALSE], na.rm=TRUE))) + (min.cat - 1)
       ## compute significance only for the AMCC
       x3 <- factor(ifelse (x2 <= mccix, "1", "0"))
       y3 <- factor(ifelse (y2 <= mccix, "1", "0"))
       if (nperm > 0) {
-        mm[mccix, "p"] <- mcc(x=x3, y=y3, nperm=nperm, nthread=nthread)["p"]
+        mm[mccix, "p.value"] <- mcc(x=x3, y=y3, nperm=nperm, nthread=nthread)[["p.value"]]
         ## bonferronni correction
         # mm[mccix, "p"] <- mm[mccix, "p"] * length(x3)
       }
-      if (!is.na(mm[mccix, "p"]) && mm[mccix, "p"] > 1) { mm[mccix, "p"] <- 1 }
+      if (!is.na(mm[mccix, "p.value"]) && mm[mccix, "p.value"] > 1) { mm[mccix, "p.value"] <- 1 }
       res <- c("mcc"=mm[mccix, ], "n1"=mccix, "n2"=nrow(mm) - mccix, "n"=nrow(mm))
     } else {
       res <- c("mcc"=NA, "p"=NA, "n1"=0, "n2"=0, "n"=sum(ccix))
+      mm <- NA
     }
     names(res) <- c("mcc", "p", "n1", "n2", "n")
     return(list("amcc"=res, "mcc"=mm))
