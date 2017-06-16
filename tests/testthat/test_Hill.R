b86e7a0d6800cd5d6b651da80db4425c065bc6b8
p-smirnov <petr.smirnov@mail.utoronto.ca>
2017-06-16 18:43:44 -0400

    Incorrect quotation marks

--- tests/testthat/test_Hill.R
+++ tests/testthat/test_Hill.R
@@ -7,6 +7,6 @@ test_that("Returns right maths",{
     expect_equal(.Hill(0, c(1, 0, -Inf)), 0)
     expect_equal(.Hill(0, c(0, 0, 0)), 1/2)
     expect_equal(.Hill(0, c(1, 0, Inf)), 1)
-    expect_equal(.Hill(-Inf, c(1, 0.2, 1)), 0)
+    expect_equal(.Hill(-Inf, c(1, 0.2, 1)), 1)
     expect_equal(.Hill(Inf, c(1, 0.2, 1)), 0.2)
 })
--- tests/testthat/test_logLogisticRegression.R
+++ tests/testthat/test_logLogisticRegression.R
@@ -15,6 +15,6 @@ test_that("Stuff works",{
     expect_error(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), precision = 0)) #should complain
     expect_error(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), scale = 0)) #should complain
     expect_error(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), lower_bounds = c(0, 0, 0), upper_bounds = c(1, 1, -1))) #should complain
-    expect_error(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), family = “The Addams Family”)) #should complain
+    expect_error(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), family = "The Addams Family")) #should complain
     logLogisticRegression(c(1, 2, 3), c(50, 60, 70)) #should run with no objections
 })
