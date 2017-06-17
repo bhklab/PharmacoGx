abbd2dc4ead4f52135eae9a5ab17422aedae836d
p-smirnov <petr.smirnov@mail.utoronto.ca>
2017-06-17 14:09:54 -0400

    Added some more of marks basic checks

--- tests/testthat/test_logLogisticRegression.R
+++ tests/testthat/test_logLogisticRegression.R
@@ -22,4 +22,6 @@ test_that("Errors are checked.",{
 test_that("Values returned as expected (previous runs of function).",{
 	expect_equal(logLogisticRegression(c(1, 2, 3), c(50, 60, 70)), list(HS = 1.22388251239748, E_inf = 59.9999882231278, EC50 = 1e-06)) #should run with no objections
     expect_equal(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), family="Cauchy"), list(HS = 1.08959673823682, E_inf = 60.0004126580051, EC50 = 3.39074306428225e-06)) #should run with no objections
+	expect_equal(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), trunc=FALSE), list(HS = 1.22388251239748, E_inf = 59.9999882231278, EC50 = 1e-06)) #should run with no objections
+    expect_equal(logLogisticRegression(c(1, 2, 3), c(50, 60, 70), family="Cauchy", trunc=FALSE), list(HS = 1.08959673823682, E_inf = 60.0004126580051, EC50 = 3.39074306428225e-06)) #should run with no objections
 })
