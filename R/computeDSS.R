84a7a1dfa96852c8fc1c4774aad0ff614366fcad
markcfreeman <marky.freeman@gmail.com>
2017-07-14 13:30:19 -0400

    - added 'censor' flag to computeDSS

--- R/computeDSS.R
+++ R/computeDSS.R
@@ -6,14 +6,13 @@ computeDSS <- function(concentration,
                        viability_as_pct = TRUE,
                        trunc = TRUE,
                        verbose = TRUE,
-                       dss_type = 3
+                       dss_type = 3,
+                       censor = FALSE
                        #, ...
 ) {
   
   if(missing(concentration)){
-    
     stop("The concentration values to integrate over must always be provided.")
-    
   }
   if (missing(Hill_fit)) {
     
@@ -40,9 +39,10 @@ computeDSS <- function(concentration,
                                conc_as_log = conc_as_log,
                                viability_as_pct = viability_as_pct,
                                trunc = trunc,
-                               verbose = verbose)
+                               verbose = verbose) #is this coercing the concentration to log?
     pars <- cleanData[["Hill_fit"]]
     concentration <- cleanData[["log_conc"]]
+    
   }
   
   if(pars[[3]] > max(concentration)) {
@@ -60,6 +60,14 @@ computeDSS <- function(concentration,
   
   x1 <- max(x1, min(concentration))
   
+  if (censor) {
+    if (pars[[2]] > 50) {
+      return(NA)
+    } else if (all(concentration < pars[[3]])) {
+      return(0)
+    }
+  }
+  
   AUC <- computeAUC(concentration = c(x1, x2), Hill_fit = unlist(pars), conc_as_log = TRUE, viability_as_pct = TRUE, verbose = verbose, trunc = trunc)
   
   
@@ -67,7 +75,7 @@ computeDSS <- function(concentration,
   if (dss_type == 1) {
     return(DSS)
   }
-  DSS <- DSS / 2
+  DSS <- DSS / log(100 - pars[[2]])
   if (dss_type == 2) {
     return(DSS)
   }
