d9ab036db23c2379272b7ffa85e9b95dc722d360
ChristopherEeles <christopher.eeles@outlook.com>
2019-10-13 14:42:18 -0400

    Fix: typo in function prevents build

--- R/geneDrugSensitivity.R
+++ R/geneDrugSensitivity.R
@@ -180,7 +180,7 @@ rr0 <- tryCatch(try(lm(formula(paste(ff0, "~ . -x", sep=" ")), data=dd)),
 }
   
   
-  if (!is(rr0,"try-error") && !is(rr1, "try-error") & !is(rr, "character") && !is(rr1, "character")) {
+  if (!is(rr0,"try-error") && !is(rr1, "try-error") & !is(rr0, "character") && !is(rr1, "character")) {
     rr <- summary(rr1)
 
     if(any(unlist(lapply(drugpheno,is.factor)))){
