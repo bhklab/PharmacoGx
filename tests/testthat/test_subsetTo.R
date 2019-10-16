003c7b52d9f4da5a2311981f89ede1cb0f341a92
ChristopherEeles <christopher.eeles@outlook.com>
2019-10-16 18:58:58 -0400

    Fix: Timeout - tests from >10 min to 3 min

--- tests/testthat/test_subsetTo.R
+++ tests/testthat/test_subsetTo.R
@@ -2,10 +2,9 @@ library(PharmacoGx)
 
 context("Checking subsetTo.")
 
-
 test_that("Intersection result did not change since last time", {
 	data(CMAPsmall)
-	CMAPsmaller <- subsetTo(CMAPsmall, drugs=drugNames(CMAPsmall), cells=cellNames(CMAPsmall))
-	expect_equal(CMAPsmaller, CMAPsmall)
+	CMAPsmaller <- subsetTo(CMAPsmall, drugs=drugNames(CMAPsmall)[1:5], cells=cellNames(CMAPsmall)[1:5])
+	expect_equal_to_reference(CMAPsmaller, "subsetTo.CMAPsmaller.rds")
 })
 
