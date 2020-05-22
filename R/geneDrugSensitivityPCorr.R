cc1ac8a99580ec71e7a65b59faef3b1aa3c0cdba
Petr Smirnov <psmirnov2000@gmail.com>
2020-05-22 13:54:17 -0400

    fixing an edge case where incomplete data caused full factor levels to be dropped, part 2

--- R/geneDrugSensitivityPCorr.R
+++ R/geneDrugSensitivityPCorr.R
@@ -204,11 +204,11 @@ geneDrugSensitivityPCorr <- function(x, type, batch, drugpheno,
   dd <- data.frame(drugpheno, "x"=xx)
 
   ## control for tissue type
-  if(length(sort(unique(type))) > 1) { 
+  if(length(sort(unique(type[ccix]))) > 1) { 
     dd <- cbind(dd, type=type[ccix])
   }
   ## control for batch
-  if(length(sort(unique(batch))) > 1) {
+  if(length(sort(unique(batch[ccix]))) > 1) {
     dd <- cbind(dd, batch=batch[ccix])
   }
   ## control for duration
