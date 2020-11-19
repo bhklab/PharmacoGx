e1a0f4caa9c7aeeba2905a3f46cf0c0eebe8a49a
ChristopherEeles <christopher.eeles@outlook.com>
2020-11-19 11:48:10 -0500

    Fix: Updated `downloadPertSig` function and documentation to work with previous modifications of `availablePSets`

--- R/downloadSignatures.R
+++ R/downloadSignatures.R
@@ -11,39 +11,43 @@
 #' downloadPertSig("CMAP")
 #' }
 #'  
-#' @param name \code{Character} string, the name of the PharmacoSet for which to
-#'   download signatures. The name should match the names returned in the
-#'   availablePSets table.
-#' @param saveDir \code{Character} string with the folder path where the 
+#' @param name A \code{character} string, the name of the PharmacoSet for which
+#'   to download signatures. The name should match the names returned in the
+#'   `Dataset Name` column of `availablePSets(canonical=FALSE)`.
+#' @param saveDir A \code{character} string with the folder path where the
 #'   PharmacoSet should be saved. Defaults to \code{"./PSets/Sigs/"}. Will
 #'   create directory if it does not exist.
 #' @param myfn \code{character} string, the file name to save the dataset under
 #' @param verbose \code{bool} Should status messages be printed during download.
 #'   Defaults to TRUE.
+#'
 #' @return An array type object contaning the signatures
+#'
 #' @export
 #' @import downloader
+downloadPertSig <- function(name, saveDir=file.path(".", "PSets", "Sigs"),
+    myfn=NULL, verbose=TRUE) {
 
-downloadPertSig <- function(name, saveDir=file.path(".", "PSets", "Sigs"), myfn=NULL, verbose=TRUE) {
-  
-  
-  pSetTable <- availablePSets()
-  
-  whichx <- match(name, pSetTable[,1])
-  if (is.na(whichx)){
-    stop('Unknown Dataset. Please use the availablePSet function for the table of available PharamcoSets.')
-  }
-  if (!pSetTable[whichx,"Dataset.Type"]%in%c("perturbation", "both")){
-    stop('Signatures are available only for perturbation type datasets')
-  }
-  
-  if(!file.exists(saveDir)) {
-    dir.create(saveDir, recursive=TRUE)
-  }
 
-  myfn <- paste(name, "_signatures.RData", sep="")
-  
-  downloader::download(file.path("https://www.pmgenomics.ca/bhklab/sites/default/files/downloads/", myfn), destfile=file.path(saveDir, myfn), quiet=!verbose)
-  sig <- load(file.path(saveDir, myfn))
-  return(get(sig))
+    pSetTable <- availablePSets(canonical=FALSE)
+
+    whichx <- match(name, pSetTable[,1])
+    if (is.na(whichx)){
+        stop('Unknown Dataset. Please use the availablePSet function for the table of available PharamcoSets.')
+    }
+    if (!pSetTable[whichx,"type"] %in% c("perturbation", "both")){
+        stop('Signatures are available only for perturbation type datasets')
+    }
+
+    if(!file.exists(saveDir)) {
+        dir.create(saveDir, recursive=TRUE)
+    }
+
+    myfn <- paste(name, "_signatures.RData", sep="")
+
+    downloader::download(file.path(
+        "https://www.pmgenomics.ca/bhklab/sites/default/files/downloads/", myfn),
+        destfile=file.path(saveDir, myfn), quiet=!verbose, mode='wb')
+    sig <- load(file.path(saveDir, myfn))
+    return(get(sig))
 }
