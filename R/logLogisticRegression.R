dbeb126dc7959e56e43e88f00838d68102442b70
Christopher Eeles <christopher.eeles@outlook.com>
2022-10-06 12:55:31 -0400

    Switch LICENSE to GPLv3

--- R/logLogisticRegression.R
+++ R/logLogisticRegression.R
@@ -1,6 +1,6 @@
 #' Fits curves of the form E = E_inf + (1 - E_inf)/(1 + (c/EC50)^HS) to dose-response data points (c, E) given by the user
 #' and returns a vector containing estimates for HS, E_inf, and EC50.
-#' 
+#'
 #' By default, logLogisticRegression uses an L-BFGS algorithm to generate the fit. However, if
 #' this fails to converge to solution, logLogisticRegression samples lattice points throughout the parameter space.
 #' It then uses the lattice point with minimal least-squares residual as an initial guess for the optimal parameters,
@@ -8,16 +8,16 @@
 #' PatternSearch algorithm to fit a log-logistic curve to the data.
 #'
 #' @examples
-#' dose <- c(0.0025,0.008,0.025,0.08,0.25,0.8,2.53,8) 
+#' dose <- c(0.0025,0.008,0.025,0.08,0.25,0.8,2.53,8)
 #' viability <- c(108.67,111,102.16,100.27,90,87,74,57)
 #' computeAUC(dose, viability)
-#' 
+#'
 #' @param conc `numeric` is a vector of drug concentrations.
 #' @param viability `numeric` is a vector whose entries are the viability values observed in the presence of the
 #' drug concentrations whose logarithms are in the corresponding entries of the log_conc, where viability 0
-#' indicates that all cells died, and viability 1 indicates that the drug had no effect on the cells.   
+#' indicates that all cells died, and viability 1 indicates that the drug had no effect on the cells.
 #' @param density `numeric` is a vector of length 3 whose components are the numbers of lattice points per unit
-#' length along the HS-, E_inf-, and base-10 logarithm of the EC50-dimensions of the parameter space, respectively. 
+#' length along the HS-, E_inf-, and base-10 logarithm of the EC50-dimensions of the parameter space, respectively.
 #' @param step `numeric` is a vector of length 3 whose entries are the initial step sizes in the HS, E_inf, and
 #' base-10 logarithm of the EC50 dimensions, respectively, for the PatternSearch algorithm.
 #' @param precision is a positive real number such that when the ratio of current step size to initial step
@@ -33,22 +33,22 @@
 #' dose-response curves to the data. Otherwise, if "normal", uses MLE with a gaussian assumption of errors
 #' @param median_n If the viability points being fit were medians of measurements, they are expected to follow a median of \code{family}
 #' distribution, which is in general quite different from the case of one measurement. Median_n is the number of measurements
-#' the median was taken of. If the measurements are means of values, then both the Normal and the Cauchy distributions are stable, so means of 
+#' the median was taken of. If the measurements are means of values, then both the Normal and the Cauchy distributions are stable, so means of
 #' Cauchy or Normal distributed variables are still Cauchy and normal respectively.
 #' @param conc_as_log `logical`, if true, assumes that log10-concentration data has been given rather than concentration data,
 #' and that log10(EC50) should be returned instead of EC50.
 #' @param viability_as_pct `logical`, if false, assumes that viability is given as a decimal rather
 #' than a percentage, and that E_inf should be returned as a decimal rather than a percentage.
-#' @param trunc `logical`, if true, causes viability data to be truncated to lie between 0 and 1 before 
+#' @param trunc `logical`, if true, causes viability data to be truncated to lie between 0 and 1 before
 #' curve-fitting is performed.
 #' @param verbose `logical`, if true, causes warnings thrown by the function to be printed.
-#' @return A list containing estimates for HS, E_inf, and EC50. It is annotated with the attribute Rsquared, which is the R^2 of the fit. 
-#' Note that this is calculated using the values actually used for the fit, after truncation and any transform applied. With truncation, this will be 
-#' different from the R^2 compared to the variance of the raw data. This also means that if all points were truncated down or up, there is no variance 
-#' in the data, and the R^2 may be NaN. 
+#' @return A list containing estimates for HS, E_inf, and EC50. It is annotated with the attribute Rsquared, which is the R^2 of the fit.
+#' Note that this is calculated using the values actually used for the fit, after truncation and any transform applied. With truncation, this will be
+#' different from the R^2 compared to the variance of the raw data. This also means that if all points were truncated down or up, there is no variance
+#' in the data, and the R^2 may be NaN.
 #'
 #' @export
-#' 
+#'
 #' @importFrom CoreGx .meshEval .residual
 #' @importFrom stats optim dcauchy dnorm pcauchy rcauchy rnorm pnorm integrate
 logLogisticRegression <- function(conc,
@@ -88,37 +88,37 @@ logLogisticRegression <- function(conc,
     print(step)
     stop("Step vector contains elements which are not positive real numbers.")
   }
-  
+
   if (prod(is.finite(precision)) != 1) {
     print(precision)
     stop("Precision value is not a real number.")
   }
-  
+
   if (prod(is.finite(lower_bounds)) != 1) {
     print(lower_bounds)
     stop("Lower bounds vector contains elements which are not real numbers.")
   }
-  
+
   if (prod(is.finite(upper_bounds)) != 1) {
     print(upper_bounds)
     stop("Upper bounds vector contains elements which are not real numbers.")
   }
-  
+
   if (prod(is.finite(density)) != 1) {
     print(density)
     stop("Density vector contains elements which are not real numbers.")
   }
-  
+
   if (is.finite(scale) == FALSE) {
     print(scale)
     stop("Scale is not a real number.")
   }
-  
+
   if (is.character(family) == FALSE) {
     print(family)
     stop("Cauchy flag is not a string.")
   }
-  
+
   if (length(density) != 3){
     stop("Density parameter needs to have length of 3, for HS, Einf, EC50")
   }
@@ -127,34 +127,34 @@ logLogisticRegression <- function(conc,
     stop("There can only be a integral number of samples to take a median of. Check your setting of median_n parameter, it is not an integer")
   }
 
-  
+
   if (min(upper_bounds - lower_bounds) < 0) {
     print(rbind(lower_bounds, upper_bounds))
     stop("Upper bounds on parameters do not exceed lower bounds.")
   }
-  
-  
-  
+
+
+
   if (min(density) <= 0) {
     print(density)
     stop("Lattice point density vector contains negative values.")
   }
-  
+
   if (precision <= 0) {
     print(precision)
     stop("Negative precision value.")
   }
-  
+
   if (min(step) <= 0) {
     print(step)
     stop("Step vector contains nonpositive numbers.")
   }
-  
+
   if (scale <= 0) {
     print(scale)
     stop("Scale parameter is a nonpositive number.")
   }
- 
+
 
 
 
@@ -187,14 +187,14 @@ logLogisticRegression <- function(conc,
 
   log_conc <- cleanData[["x"]]
   viability <- cleanData[["y"]]
-  
-  
+
+
   #ATTEMPT TO REFINE GUESS WITH L-BFGS OPTIMIZATION
   # tryCatch(
   gritty_guess <- c(pmin(pmax(1, lower_bounds[1]), upper_bounds[1]),
                     pmin(pmax(min(viability), lower_bounds[2]), upper_bounds[2]),
                     pmin(pmax(log_conc[which.min(abs(viability - 1/2))], lower_bounds[3]), upper_bounds[3]))
-  
+
 
   guess <- CoreGx::.fitCurve(x = log_conc,
                               y = viability,
